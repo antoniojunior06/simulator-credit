c4495c47c475688e4a04d0f5e5e8c84f6973584c
antoniojr06 <antoniojr06@gmail.com>
2023-06-16 21:32:09 -0300

    novo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+
+
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule { }
